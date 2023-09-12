eed646007a82c07ecdba3d596508d8f9b44d3513
Alex Kist <akist0707@gmail.com>
2023-09-12 11:02:14 -0500

    Update skills section to break down skills into subsections

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -4,26 +4,44 @@
         <h3>Skills</h3>
         <span id="underline"></span>
     </header>
-    <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Sass</li>
-        <li>Bootstrap</li>
-        <li>Vuetify</li>
-        <li>JavaScript</li>
-        <li>TypeScript</li>
-        <li>React</li>
-        <li>Next.js</li>
-        <li>Redux</li>
-        <li>Vue</li>
-        <li>Pinia</li>
-        <li>Vue Query</li>
-        <li>Astro</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>MongoDB</li>
-        <li>MySQL</li>
-        <li>Strapi</li>
+    <ul class="skills-wrapper">
+        <li class="skills-section">
+            <h4>Front End</h4>
+            <ul class="skills-front">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+                <li>Vuetify</li>
+                <li>Chakra UI</li>
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>React</li>
+                <li>Next.js</li>
+                <li>Redux</li>
+                <li>Vue</li>
+                <li>Pinia</li>
+                <li>Vue Query</li>
+                <li>Astro</li>
+            </ul>
+        </li>
+        <li class="skills-section">
+            <h4>Back End</h4>
+            <ul class="skills-back">
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>MongoDB</li>
+                <li>SQLite</li>
+                <li>Strapi</li>
+            </ul>
+        </li>
+        <li class="skills-section">
+            <h4>Tools</h4>
+            <ul class="skills-tools">
+                <li>Git</li>
+                <li>GitHub</li>
+            </ul>
+        </li>
     </ul>
 </section>
 </template>
@@ -95,111 +113,149 @@ export default defineComponent({
         }
     }
 
-    ul {
+    .skills-wrapper {
         /* Positioning */
         display: flex;
-            flex-wrap: wrap;
+            flex-direction: column;
             justify-content: center;
             align-items: center;
-            gap: 20px;
+            gap: 50px;
 
         /* Visual */
         list-style-type: none;
         font-size: 24px;
 
-        li {   
+        .skills-section {   
             display: flex;
                 flex-direction: column;
                 justify-content: center;
                 align-items: center;
                 gap: 10px;
 
-            &::after {
-                background-size: contain;
-                    background-repeat: no-repeat;
-                    background-position: center;
-                content: '';
-                height: 96px;
-                width: 96px;
-            }
+            ul {
+                display: flex;
+                    flex-wrap: wrap;
+                    justify-content: center;
+                    align-items: center;
+                    gap: 20px;
 
-            &:nth-child(1)::after {
-                background-image: url('/images/icons/html.svg');
-            }
+                /* Visual */
+                list-style-type: none;
+                font-size: 24px;
 
-            &:nth-child(2)::after {
-                background-image: url('/images/icons/css.svg');
-            }
+                li {
+                    display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+                        align-items: center;
+                        gap: 10px;
 
-            &:nth-child(3)::after {
-                background-image: url('/images/icons/sass.svg');
+                    &::after {
+                        background-size: contain;
+                            background-repeat: no-repeat;
+                            background-position: center;
+                        content: '';
+                        height: 96px;
+                        width: 96px;
+                    }
+                }
             }
+        }
+    }
 
-            &:nth-child(4)::after {
-                background-image: url('/images/icons/bootstrap.svg');
-            }
+    .skills-front {
+        li:nth-child(1)::after {
+            background-image: url('/images/icons/html.svg');
+        }
 
-            &:nth-child(5)::after {
-                background-image: url('/images/icons/vuetify.svg');
-            }
+        li:nth-child(2)::after {
+            background-image: url('/images/icons/css.svg');
+        }
 
-            &:nth-child(6)::after {
-                background-image: url('/images/icons/javascript.svg');
-            }
+        li:nth-child(3)::after {
+            background-image: url('/images/icons/sass.svg');
+        }
 
-            &:nth-child(7)::after {
-                background-image: url('/images/icons/typescript.svg');
-            }
+        li:nth-child(4)::after {
+            background-image: url('/images/icons/bootstrap.svg');
+        }
 
-            &:nth-child(8)::after {
-                background-image: url('/images/icons/react.svg');
-            }
+        li:nth-child(5)::after {
+            background-image: url('/images/icons/vuetify.svg');
+        }
 
-            &:nth-child(9)::after {
-                background-image: url('/images/icons/nextjs.svg');
-                filter: invert(1);
-            }
+        li:nth-child(6)::after {
+            background-image: url('/images/icons/chakra-ui.png');
+        }
 
-            &:nth-child(10)::after {
-                background-image: url('/images/icons/redux.svg');
-            }
+        li:nth-child(7)::after {
+            background-image: url('/images/icons/javascript.svg');
+        }
 
-            &:nth-child(11)::after {
-                background-image: url('/images/icons/vue.svg');
-            }
+        li:nth-child(8)::after {
+            background-image: url('/images/icons/typescript.svg');
+        }
 
-            &:nth-child(12)::after {
-                background-image: url('/images/icons/pinia.svg');
-            }
+        li:nth-child(9)::after {
+            background-image: url('/images/icons/react.svg');
+        }
 
-            &:nth-child(13)::after {
-                background-image: url('/images/icons/vue-query.svg');
-            }
+        li:nth-child(10)::after {
+            background-image: url('/images/icons/nextjs.svg');
+            filter: invert(1);
+        }
 
-            &:nth-child(14)::after {
-                background-image: url('/images/icons/astro.svg');
-            }
+        li:nth-child(11)::after {
+            background-image: url('/images/icons/redux.svg');
+        }
 
-            &:nth-child(15)::after {
-                background-image: url('/images/icons/nodejs.svg');
-            }
+        li:nth-child(12)::after {
+            background-image: url('/images/icons/vue.svg');
+        }
 
-            &:nth-child(16)::after {
-                background-image: url('/images/icons/expressjs.svg');
-                filter: invert(1);
-            }
+        li:nth-child(13)::after {
+            background-image: url('/images/icons/pinia.svg');
+        }
 
-            &:nth-child(17)::after {
-                background-image: url('/images/icons/mongodb.svg');
-            }
+        li:nth-child(14)::after {
+            background-image: url('/images/icons/vue-query.svg');
+        }
 
-            &:nth-child(18)::after {
-                background-image: url('/images/icons/mysql.svg');
-            }
+        li:nth-child(15)::after {
+            background-image: url('/images/icons/astro.svg');
+        }
+    }
 
-            &:nth-child(19)::after {
-                background-image: url('/images/icons/strapi.svg');
-            }
+    .skills-back {
+        li:nth-child(1)::after {
+            background-image: url('/images/icons/nodejs.svg');
+        }
+
+        li:nth-child(2)::after {
+            background-image: url('/images/icons/expressjs.svg');
+            filter: invert(1);
+        }
+
+        li:nth-child(3)::after {
+            background-image: url('/images/icons/mongodb.svg');
+        }
+
+        li:nth-child(4)::after {
+            background-image: url('/images/icons/sqlite.svg');
+        }
+
+        li:nth-child(5)::after {
+            background-image: url('/images/icons/strapi.svg');
+        }
+    }
+
+    .skills-tools {
+        li:nth-child(1)::after {
+            background-image: url('/images/icons/git.svg');
+        }
+
+        li:nth-child(2)::after {
+            background-image: url('/images/icons/github.svg');
         }
     }
 }
